525590c29f69d6846337955385f4f2e555111557
Gaspar Peter <peti@jula.unixoft.hu>
2025-09-28 21:39:46 +0200

    Remove build artifacts
    - build dir: This directory is generated, not needed here
    - lib itself
    - Also add artifacts to .gitignore

--- build/CMakeFiles/PSToolbox.dir/src/Gas.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/PSToolbox.dir/src/Gas.cpp.o: \
-  /Users/hoscsaba/Documents/GitHub/PSToolbox/src/Gas.cpp \
-  /Users/hoscsaba/Documents/GitHub/PSToolbox/src/Gas.h
--- build/CMakeFiles/PSToolbox.dir/src/Units.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/PSToolbox.dir/src/Units.cpp.o: \
-  /Users/hoscsaba/Documents/GitHub/PSToolbox/src/Units.cpp \
-  /Users/hoscsaba/Documents/GitHub/PSToolbox/src/Units.h
